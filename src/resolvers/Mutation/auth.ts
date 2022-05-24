d3739367a9c180449a8c83371e761840c1e10998
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-05-24 10:17:34 +0200

    Add signup resolver

--- src/resolvers/Mutation/auth.ts
+++ src/resolvers/Mutation/auth.ts
@@ -1,4 +1,5 @@
 import { Context } from '../../index';
+import validator from 'validator';
 
 interface SignupArgs {
   email: string;
@@ -7,10 +8,61 @@ interface SignupArgs {
   bio: string;
 }
 
+interface UserPayload {
+  userErrors: {
+    message: string;
+  }[];
+  user: null;
+}
+
 export const authResolvers = {
-  signup: (
+  signup: async (
     parent: any,
     { email, name, password, bio }: SignupArgs,
     { prisma }: Context
-  ) => {},
+  ): Promise<UserPayload> => {
+    const isEmail = validator.isEmail(email);
+
+    if (!isEmail) {
+      return {
+        userErrors: [
+          {
+            message: 'Invalid email.',
+          },
+        ],
+        user: null,
+      };
+    }
+
+    const isValidPassword = validator.isLength(password, {
+      min: 5,
+    });
+
+    if (!isValidPassword) {
+      return {
+        userErrors: [
+          {
+            message: 'Password must be at least 5 characters long.',
+          },
+        ],
+        user: null,
+      };
+    }
+
+    if (!name || !bio) {
+      return {
+        userErrors: [
+          {
+            message: 'Missing name or bio.',
+          },
+        ],
+        user: null,
+      };
+    }
+
+    return {
+      userErrors: [],
+      user: null,
+    };
+  },
 };
