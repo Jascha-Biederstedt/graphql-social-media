a564b18be4f3f6ce00a97e3e0c18bf86fcb06652
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-05-26 16:06:15 +0200

    Add the me query

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,6 +1,16 @@
 import { Context } from '../index';
 
 export const Query = {
+  me: async (parent: any, args: any, { prisma, userInfo }: Context) => {
+    if (!userInfo) return null;
+
+    return await prisma.user.findUnique({
+      where: {
+        id: userInfo.userId,
+      },
+    });
+  },
+
   posts: (parent: any, args: any, { prisma }: Context) => {
     return prisma.post.findMany({
       orderBy: [
