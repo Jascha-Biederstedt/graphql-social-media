691e27da8d134af5f8da7d111092eab1f37fd16e
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-05-30 14:32:10 +0200

    Implement conditional rendering based on authentication

--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -24,15 +24,24 @@ export const Query = {
     });
   },
 
-  profile: (
+  profile: async (
     parent: any,
     { userId }: { userId: string },
-    { prisma }: Context
+    { prisma, userInfo }: Context
   ) => {
-    return prisma.profile.findMany({
+    const isMyProfile = Number(userId) === userInfo?.userId;
+
+    const profile = await prisma.profile.findMany({
       where: {
         userId: Number(userId),
       },
     });
+
+    if (!profile) return null;
+
+    return {
+      ...profile,
+      isMyProfile,
+    };
   },
 };
